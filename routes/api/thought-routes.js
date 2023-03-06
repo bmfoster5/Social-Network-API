f04e5fe4066d7b9b44e3579dd3dd2a0cf82fc881
bmfoster5 <foster.brandon96@gmail.com>
2023-03-06 13:53:25 -0500

    created thought and user routes and controllers

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,21 +1,19 @@
 //EDIT
 const router = require('express').Router();
 const {
-  getCourses,
-  getSingleCourse,
-  createCourse,
-  updateCourse,
-  deleteCourse,
-} = require('../../controllers/courseController.js');
+  getThought,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addThought,
+  removeThought,
+} = require('../../controllers/thought-controller');
 
-// /api/courses
-router.route('/').get(getCourses).post(createCourse);
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
 
-// /api/courses/:courseId
-router
-  .route('/:courseId')
-  .get(getSingleCourse)
-  .put(updateCourse)
-  .delete(deleteCourse);
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 module.exports = router;
