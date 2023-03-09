5a59991da2b8536095362273623a242e5c5b0c82
bmfoster5 <foster.brandon96@gmail.com>
2023-03-09 11:03:09 -0500

    created Reaction routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,5 +4,6 @@ const thoughtRoutes = require('./thought-routes');
 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
+router.use('/reactions', reactionRoutes);
 
 module.exports = router;
---
+++ routes/api/reaction-routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const {
+  getReaction,
+  getSingleReaction,
+  createReaction,
+  deleteReaction,
+  
+} = require('../../controllers/reactionController.js');
+
+// /api/reactions
+router.route('/').get(getReaction).post(createReaction);
+
+// /api/reactions/:reactionId
+router.route('/:reactionId').get(getSingleReaction).put(updateReaction).delete(deleteReaction);
+
+module.exports = router;
